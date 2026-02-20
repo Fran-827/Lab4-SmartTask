fb01f9c28bf76407b27524864922c70327fdb144
Francis <remolarfrancis@gmail.com>
2026-02-20 23:06:21 +0800

    added all the files

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -15,9 +15,9 @@ namespace SmartTask
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
-#if DEBUG
+
     		builder.Logging.AddDebug();
-#endif
+
 
             return builder.Build();
         }
